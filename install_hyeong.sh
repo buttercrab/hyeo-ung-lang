d68aa583b76d24eff7a7d1040d200afa1bf6844b
buttercrab <jaeyong0201@gmail.com>
2020-02-29 00:56:30 +0900

    edited install_hyeong.sh

--- install_hyeong.sh
+++ install_hyeong.sh
@@ -1,6 +1,6 @@
-printf "\e[01;36m==> cloning repo to ~/.hyeong\e[0m\n"
+printf "\e[01;36m==> cloning repo to ~/.hyeong\e[0m\n\n\n"
 git clone https://github.com/buttercrab/hyeo-ung-lang ~/.hyeong
-printf "\n\e[01;36m==> building hyeong\e[0m\n"
+printf "\n\e[01;36m==> building hyeong\e[0m\n\n\n"
 cargo build --manifest-path="$HOME"/.hyeong/Cargo.toml
 printf "\n\e[01;36m==> done!\e[0m\n"
 echo "run next to add to your PATH:"
