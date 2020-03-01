01444a8de76e6b5054c8f4917b44025dcec2cb96
buttercrab <jaeyong0201@gmail.com>
2020-03-01 21:17:56 +0900

    changed to release

--- install_hyeong.sh
+++ install_hyeong.sh
@@ -1,7 +1,7 @@
 printf "\e[01;36m==> cloning repo to ~/.hyeong\e[0m \n"
 git clone https://github.com/buttercrab/hyeo-ung-lang ~/.hyeong
 printf "\n\e[01;36m==> building hyeong\e[0m \n"
-cargo build --manifest-path="$HOME"/.hyeong/Cargo.toml
+cargo build --manifest-path="$HOME"/.hyeong/Cargo.toml --release
 printf "\n\e[01;36m==> done!\e[0m \n"
 echo "run next to add to your PATH:"
-echo "    echo \"export PATH=\$PATH:~/.hyeong/target/debug\" >> ~/.bashrc"
+echo "    echo \"export PATH=\$PATH:~/.hyeong/target/release\" >> ~/.bashrc"
