80874c1348ebc4823d9cbf0db5bac3a29eef9c76
buttercrab <jaeyong0201@gmail.com>
2020-03-02 16:10:05 +0900

    added spaces between lines

--- install_hyeong.sh
+++ install_hyeong.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 printf "\e[01;36m==> cloning repo to ~/.hyeong\e[0m \n"
 git clone https://github.com/buttercrab/hyeo-ung-lang ~/.hyeong
+
 printf "\n\e[01;36m==> building hyeong\e[0m \n"
 cargo build --manifest-path="$HOME"/.hyeong/Cargo.toml --release
+
 printf "\n\e[01;36m==> done!\e[0m \n"
 echo "run next to add to your PATH:"
 echo "    echo \"export PATH=\$PATH:~/.hyeong/target/release\" >> ~/.bashrc"
