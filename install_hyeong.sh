4a87ea5d43e3ff85f4a01ba386a0a80c2f5f8b39
buttercrab <jaeyong0201@gmail.com>
2020-03-08 19:15:41 +0900

    edited install_hyeong.sh

--- install_hyeong.sh
+++ install_hyeong.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-printf "\e[01;36m==> cloning repos to ~/.hyeong\e[0m \n"
+printf "\e[01;36m==> cloning repo to ~/.hyeong/hyeong\e[0m \n"
 git clone https://github.com/buttercrab/hyeo-ung-lang ~/.hyeong/hyeong || (cd ~/.hyeong/hyeong && git pull)
 
 printf "\n\e[01;36m==> building hyeong\e[0m \n"
 cargo build --manifest-path="$HOME"/.hyeong/hyeong/Cargo.toml --release
+
+printf "\n\e[01;36m==> making directory for building hyeong code\e[0m \n"
+cd ~/.hyeong && cargo new hyeong-build
+cp ~/.hyeong/hyeong/src/number.rs ~/.hyeong/hyeong-build/src/
+cp ~/.hyeong/hyeong/src/big_number.rs ~/.hyeong/hyeong-build/src/
+
+printf "\n\e[01;36m==> test build for building hyeong code\e[0m \n"
+cargo build --manifest-path="$HOME"/.hyeong/hyeong-build/Cargo.toml --release
 
 printf "\n\e[01;36m==> done!\e[0m \n"
 
