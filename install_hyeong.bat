5dc4507541efddfbddec60c88232cf46b8bb74f7
Godthink_un <44489551+myeongjinshin@users.noreply.github.com>
2020-03-10 15:50:24 +0900

    Update install_hyeong.bat

--- install_hyeong.bat
+++ install_hyeong.bat
@@ -4,17 +4,18 @@ print "\e[01;36m==> cloning repo to ~/.hyeong/hyeong\e[0m \n"
 git clone https://github.com/buttercrab/hyeo-ung-lang ~/.hyeong/hyeong || (cd %USERPROFILE%/.hyeong/hyeong && git pull)
 
 print "\n\e[01;36m==> building hyeong\e[0m \n"
-cargo build --manifest-path="$HOME"/.hyeong/hyeong/Cargo.toml --release
+cargo build --manifest-path=%USERPROFILE%/.hyeong/hyeong/Cargo.toml --release
 
 print "\n\e[01;36m==> making directory for building hyeong code\e[0m \n"
-if [ ! -d "$HOME/.hyeong/hyeong-build/" ]; then
-  cd %USERPROFILE%/.hyeong && cargo new hyeong-build --vcs none
-fi
-copy "%USERPROFILE%/.hyeong/hyeong/src/number.rs" "%USERPROFILE%/.hyeong/hyeong-build/src/"
-copy "%USERPROFILE%/.hyeong/hyeong/src/big_number.rs" "%USERPROFILE%/.hyeong/hyeong-build/src/"
-print "pub mod big_number;\npub mod number;" > %USERPROFILE%/.hyeong/hyeong-build/src/lib.rs
+IF NOT EXIST %USERPROFILE%/.hyeong/hyeong-build/ (
+    cd %USERPROFILE%/.hyeong && cargo new hyeong-build --vcs none
+) 
 
-print "\n\e[01;36m==> test build for building hyeong code\e[0m \n"
-cargo build --manifest-path="$HOME"/.hyeong/hyeong-build/Cargo.toml --release
+COPY "%USERPROFILE%/.hyeong/hyeong/src/number.rs" "%USERPROFILE%/.hyeong/hyeong-build/src/"
+COPY "%USERPROFILE%/.hyeong/hyeong/src/big_number.rs" "%USERPROFILE%/.hyeong/hyeong-build/src/"
+PRINT "pub mod big_number;\npub mod number;" > %USERPROFILE%/.hyeong/hyeong-build/src/lib.rs
 
-print "\n\e[01;36m==> done!\e[0m \n"
+PRINT "\n\e[01;36m==> test build for building hyeong code\e[0m \n"
+cargo build --manifest-path="%USERPROFILE%"/.hyeong/hyeong-build/Cargo.toml --release
+
+PRINT "\n\e[01;36m==> done!\e[0m \n"
