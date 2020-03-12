e47a2f410032e6002012e28a22714b8409a955b6
buttercrab <jaeyong0201@gmail.com>
2020-03-12 11:47:15 +0900

    added color to cmd

--- install_without_build.cmd
+++ install_without_build.cmd
@@ -1,14 +1,14 @@
 @echo off
 
 cls
-echo ==^> cloning repo to ~/.hyeong/hyeong
+echo [01;36m==^> cloning repo to ~/.hyeong/hyeong[0m
 IF EXIST %USERPROFILE%\.hyeong\hyeong (
  cd %USERPROFILE%\.hyeong\hyeong && git pull
 ) else (
  git clone https://github.com/buttercrab/hyeo-ung-lang %USERPROFILE%\.hyeong\hyeong
 )
 
-echo ==^> making directory for building hyeong code
+echo [01;36m==^> making directory for building hyeong code[0m
 IF NOT EXIST %USERPROFILE%\.hyeong\hyeong-build (
  cd %USERPROFILE%\.hyeong && cargo new hyeong-build --vcs none
 )
@@ -17,7 +17,7 @@ COPY %USERPROFILE%\.hyeong\hyeong\src\number.rs %USERPROFILE%\.hyeong\hyeong-bui
 copy %USERPROFILE%\.hyeong\hyeong\src\big_number.rs %USERPROFILE%\.hyeong\hyeong-build\src\big_number.rs
 echo pub mod big_number;pub mod number; > %USERPROFILE%\.hyeong\hyeong-build\src\lib.rs
 
-echo ==^> test build for building hyeong code
+echo [01;36m==^> test build for building hyeong code[0m
 cargo build --manifest-path=%USERPROFILE%\.hyeong\hyeong-build\Cargo.toml --release
 
-echo ==^> done!
+echo [01;36m==^> done![0m
