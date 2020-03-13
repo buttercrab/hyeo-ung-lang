b7f10a223ccb860fef09609ec8219bfbd0b68da7
Godthink_un <44489551+myeongjinshin@users.noreply.github.com>
2020-03-13 21:26:27 +0900

    Update code_test.rs

--- tests/code_test.rs
+++ tests/code_test.rs
@@ -1,6 +1,20 @@
 ﻿#[cfg(test)]
 mod code_test {
-    use hyeong::{code, parse, io, optimize};
+    use hyeong::{code, parse, io, execute, optimize};
+    use hyeong::code::Code;
+    use hyeong::code::State;
+
+    #[test]
+    fn area_test01() {
+        let t = format!("{:?}",parse::parse("하아앗..? 흑..".to_string())[0].get_area());
+        assert_eq!("?__", t);
+    }
+
+    #[test]
+    fn area_test02() {
+        let t = format!("{:?}",parse::parse("혀엉... 핫... 형..! 흑..".to_string())[2].get_area());
+        assert_eq!("!__", t);
+    }
 
     #[test]
     fn un_opt_code_to_string_test01() {
@@ -38,5 +52,54 @@ mod code_test {
         let t1 = parse::parse("하아앗..💖 흑..".to_string())[0].get_location();
         let t2 = (1,0);
         assert_eq!(t1,t2);
+    }
+
+    #[test]
+    fn un_opt_state_test01() {
+        let parsed = &parse::parse("형. 하앙...".to_string())[0];
+        let mut ipt = io::CustomReader::new("".to_string());
+        let mut out = io::CustomWriter::new(|_| Result::Ok(()));
+        let mut err = io::CustomWriter::new(|_| Result::Ok(()));
+        let mut state = code::UnOptState::new();
+        state = execute::execute(&mut ipt, &mut out, &mut err, state, parsed);
+        let t1 = (state.get_all_stack_index()[0], state.stack_size());
+        let t2 = (3,1);
+        assert_eq!(t1, t2);
+    }
+
+    #[test]
+    fn un_opt_state_test02() {
+        let parsed = &parse::parse("형. 하앙...".to_string())[0];
+        let mut ipt = io::CustomReader::new("".to_string());
+        let mut out = io::CustomWriter::new(|_| Result::Ok(()));
+        let mut err = io::CustomWriter::new(|_| Result::Ok(()));
+        let mut state = code::UnOptState::new();
+        state = execute::execute(&mut ipt, &mut out, &mut err, state, parsed);
+        let t = format!("{:?}",state.get_all_code());
+        assert_eq!(t, "[type: 0, cnt1: 1, cnt2: 1, area: \"_\"]")
+    }
+
+    #[test]
+    fn un_opt_state_test03() {
+        let parsed = &parse::parse("형. 하앙...".to_string())[0];
+        let mut ipt = io::CustomReader::new("".to_string());
+        let mut out = io::CustomWriter::new(|_| Result::Ok(()));
+        let mut err = io::CustomWriter::new(|_| Result::Ok(()));
+        let mut state = code::UnOptState::new();
+        state = execute::execute(&mut ipt, &mut out, &mut err, state, parsed);
+        let t = state.get_all_point().len();
+        assert_eq!(t, 0);
+    }
+
+    #[test]
+    fn un_opt_state_test04() {
+        let parsed = &parse::parse("형. 하앙...".to_string())[0];
+        let mut ipt = io::CustomReader::new("".to_string());
+        let mut out = io::CustomWriter::new(|_| Result::Ok(()));
+        let mut err = io::CustomWriter::new(|_| Result::Ok(()));
+        let mut state = code::UnOptState::new();
+        state = execute::execute(&mut ipt, &mut out, &mut err, state, parsed);
+        let t = format!("{:?}",state);
+        assert_eq!(t, "current stack: 3\nstack 3: [1]\n");
     }
 }
